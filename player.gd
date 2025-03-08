7ac961b67d3c4f92912b5ac0dbd3fa576bc3b15d
oneilk10 <oneilk10@tcd.ie>
2025-03-08 16:30:26 +0100

    player moves around the screen

--- player.gd
+++ player.gd
@@ -4,7 +4,7 @@ var screen_size # Size of the game window.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,5 +24,12 @@ func _process(delta):
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+	
+	# Apply the velocity
+	position += velocity * delta
+	
+	# Optional: Clamp the player position to the screen size
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
 			
 
